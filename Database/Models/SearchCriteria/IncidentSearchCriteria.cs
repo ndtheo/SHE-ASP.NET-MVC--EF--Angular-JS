60365805226e96ef5d80cdf01a06c6e08bc2c147
ndtheo <58115166+ndtheo@users.noreply.github.com>
2019-11-27 15:36:56 +0200

    Search Criteria...

--- Database/Models/SearchCriteria/IncidentSearchCriteria.cs
+++ Database/Models/SearchCriteria/IncidentSearchCriteria.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Core.Entities.SearchCriteria
 {
     public class IncidentSearchCriteria : BaseSearchCriteria
     {
+        [Display(Name = "Incident Type")]
         public int? IncidentTypeId { get; set; }
+
+        [Display(Name = "Incident Date From")]
         public DateTime? IncidentDateFrom { get; set; }
+
+        [Display(Name = "Incident Date To")]
         public DateTime? IncidentDateTo { get; set; }
+
+        [Display(Name = "Incident Time From")]
         public DateTime? IncidentTimeFrom { get; set; }
+
+        [Display(Name = "Incident Time To")]
         public DateTime? IncidentTimeTo { get; set; }
         public string Description { get; set; }
         public string Person { get; set; }
